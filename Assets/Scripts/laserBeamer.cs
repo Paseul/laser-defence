906c91b2e014fe5840523b30c1a4a71bc1e22158
Jaehoon Lee <machianb@naver.com>
2020-04-01 17:25:03 +0900

    update tank & building's setting

--- Assets/Scripts/laserBeamer.cs
+++ Assets/Scripts/laserBeamer.cs
@@ -6,6 +6,7 @@ public class laserBeamer : MonoBehaviour
 {
     private simArea areaComponent;
     private GameObject area;
+    Rigidbody m_JointRb;
     bool m_State;
 
     public bool GetState()
@@ -24,4 +25,36 @@ public class laserBeamer : MonoBehaviour
         areaComponent.PlaceObject(gameObject, spawnAreaIndex);
         m_State = false;
     }
+
+    public void MoveAgent(float[] act)
+    {
+        var dirToGo = Vector3.zero;
+        var rotateDir = Vector3.zero;
+
+        var action = Mathf.FloorToInt(act[0]);
+        switch (action)
+        {
+            case 1:
+                dirToGo = transform.forward * 1f;
+                break;
+            case 2:
+                dirToGo = transform.forward * -1f;
+                break;
+            case 3:
+                rotateDir = transform.up * 1f;
+                break;
+            case 4:
+                rotateDir = transform.up * -1f;
+                break;
+            case 5:
+                dirToGo = transform.up * -0.1f;
+                break;
+            case 6:
+                dirToGo = transform.up * 0.1f;
+                break;
+        }
+        transform.Rotate(rotateDir, Time.deltaTime * 200f);
+        m_JointRb.AddForce(dirToGo * 2f, ForceMode.VelocityChange);
+    }
+
 }
